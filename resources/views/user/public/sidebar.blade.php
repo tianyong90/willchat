35025dda5020de3f256f20ff2e3e1cf33e33fc2a
tianyong90 <412039588@qq.com>
2016-01-21 00:36:57 +0800

    模板调整

--- resources/views/user/public/sidebar.blade.php
+++ resources/views/user/public/sidebar.blade.php
@@ -1,155 +1,172 @@
 <div class="page-sidebar-wrapper">
-  <div class="page-sidebar navbar-collapse collapse">
-    <!-- BEGIN SIDEBAR MENU -->
-    <ul class="page-sidebar-menu" id="page-sidebar-menu" data-keep-expanded="false" data-auto-scroll="true"
-        data-slide-speed="200">
-      <li class="sidebar-toggler-wrapper">
-        <div class="sidebar-toggler">
-        </div>
-      </li>
-      <li class="start">
-        <a href="{{ user_url('/') }}">
-          <i class="icon-home"></i>
-          <span class="title">用户中心</span>
-          <span class="arrow"></span>
-        </a>
-      </li>
-      <li>
-        <a href="javascript:;">
-          <i class="icon-settings"></i>
-          <span class="title">基础设置</span>
-          <span class="arrow"></span>
-        </a>
-        <ul class="sub-menu">
-          <li><a href="{{ user_url('system-function/') }}">功能管理</a></li>
-          <li><a href="{{ user_url('merchant/') }}">商家信息</a></li>
-          <li><a href="{{ user_url('payment/') }}">支付设置</a></li>
-          <!-- <li><a href="{{ user_url('/') }}">统计分析</a></li> -->
-          <li><a href="{{ user_url('menu/') }}">微信菜单</a></li>
+    <!-- BEGIN SIDEBAR -->
+    <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
+    <!-- DOC: Change data-auto-speed="200" to adjust the sub menu slide up/down speed -->
+    <div class="page-sidebar navbar-collapse collapse">
+        <!-- BEGIN SIDEBAR MENU -->
+        <!-- DOC: Apply "page-sidebar-menu-light" class right after "page-sidebar-menu" to enable light sidebar menu style(without borders) -->
+        <!-- DOC: Apply "page-sidebar-menu-hover-submenu" class right after "page-sidebar-menu" to enable hoverable(hover vs accordion) sub menu mode -->
+        <!-- DOC: Apply "page-sidebar-menu-closed" class right after "page-sidebar-menu" to collapse("page-sidebar-closed" class must be applied to the body element) the sidebar sub menu mode -->
+        <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
+        <!-- DOC: Set data-keep-expand="true" to keep the submenues expanded -->
+        <!-- DOC: Set data-auto-speed="200" to adjust the sub menu slide up/down speed -->
+        <ul class="page-sidebar-menu   " data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
+            <li class="nav-item start ">
+                <a href="javascript:;" class="nav-link nav-toggle">
+                    <i class="icon-home"></i>
+                    <span class="title">Dashboard</span>
+                    <span class="arrow"></span>
+                </a>
+                <ul class="sub-menu">
+                    <li class="nav-item start ">
+                        <a href="index.html" class="nav-link ">
+                            <i class="icon-bar-chart"></i>
+                            <span class="title">Dashboard 1</span>
+                        </a>
+                    </li>
+                    <li class="nav-item start ">
+                        <a href="dashboard_2.html" class="nav-link ">
+                            <i class="icon-bulb"></i>
+                            <span class="title">Dashboard 2</span>
+                            <span class="badge badge-success">1</span>
+                        </a>
+                    </li>
+                    <li class="nav-item start ">
+                        <a href="dashboard_3.html" class="nav-link ">
+                            <i class="icon-graph"></i>
+                            <span class="title">Dashboard 3</span>
+                            <span class="badge badge-danger">5</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="heading">
+                <h3 class="uppercase">Features</h3>
+            </li>
+            <li class="nav-item  ">
+                <a href="javascript:;" class="nav-link nav-toggle">
+                    <i class="icon-diamond"></i>
+                    <span class="title">UI Features</span>
+                    <span class="arrow"></span>
+                </a>
+                <ul class="sub-menu">
+                    <li class="nav-item  ">
+                        <a href="ui_colors.html" class="nav-link ">
+                            <span class="title">Color Library</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_blockui.html" class="nav-link ">
+                            <span class="title">Block UI</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_bootstrap_growl.html" class="nav-link ">
+                            <span class="title">Bootstrap Growl Notifications</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_notific8.html" class="nav-link ">
+                            <span class="title">Notific8 Notifications</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_toastr.html" class="nav-link ">
+                            <span class="title">Toastr Notifications</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_bootbox.html" class="nav-link ">
+                            <span class="title">Bootbox Dialogs</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_alerts_api.html" class="nav-link ">
+                            <span class="title">Metronic Alerts API</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_session_timeout.html" class="nav-link ">
+                            <span class="title">Session Timeout</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_idle_timeout.html" class="nav-link ">
+                            <span class="title">User Idle Timeout</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_modals.html" class="nav-link ">
+                            <span class="title">Modals</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_extended_modals.html" class="nav-link ">
+                            <span class="title">Extended Modals</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_tiles.html" class="nav-link ">
+                            <span class="title">Tiles</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_datepaginator.html" class="nav-link ">
+                            <span class="title">Date Paginator</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ui_nestable.html" class="nav-link ">
+                            <span class="title">Nestable List</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            
+            <li class="heading">
+                <h3 class="uppercase">Pages</h3>
+            </li>
+            <li class="nav-item  ">
+                <a href="javascript:;" class="nav-link nav-toggle">
+                    <i class="icon-basket"></i>
+                    <span class="title">eCommerce</span>
+                    <span class="arrow"></span>
+                </a>
+                <ul class="sub-menu">
+                    <li class="nav-item  ">
+                        <a href="ecommerce_index.html" class="nav-link ">
+                            <i class="icon-home"></i>
+                            <span class="title">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ecommerce_orders.html" class="nav-link ">
+                            <i class="icon-basket"></i>
+                            <span class="title">Orders</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ecommerce_orders_view.html" class="nav-link ">
+                            <i class="icon-tag"></i>
+                            <span class="title">Order View</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ecommerce_products.html" class="nav-link ">
+                            <i class="icon-graph"></i>
+                            <span class="title">Products</span>
+                        </a>
+                    </li>
+                    <li class="nav-item  ">
+                        <a href="ecommerce_products_edit.html" class="nav-link ">
+                            <i class="icon-graph"></i>
+                            <span class="title">Product Edit</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
         </ul>
-      </li>
-      {{--<li>--}}
-        {{--<a href="javascript:;">--}}
-          {{--<i class="icon-grid"></i>--}}
-          {{--<span class="title">微信管理员</span>--}}
-          {{--<span class="arrow"></span>--}}
-        {{--</a>--}}
-        {{--<ul class="sub-menu">--}}
-          {{--<li><a href="{{ user_url('/') }}">管理员列表</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">功能设置</a></li>--}}
-        {{--</ul>--}}
-      {{--</li>--}}
-      <li>
-        <a href="javascript:;">
-          <i class="icon-bubble"></i>
-          <span class="title">自动回复</span>
-          <span class="arrow"></span>
-        </a>
-        <ul class="sub-menu">
-          <li><a href="{{ user_url('/') }}">关注回复</a></li>
-          <li><a href="{{ user_url('/') }}">文本回复</a></li>
-          <li><a href="{{ user_url('/') }}">图文回复</a></li>
-          <li><a href="{{ user_url('/') }}">其它回复</a></li>
-        </ul>
-      </li>
-      {{--<li>--}}
-        {{--<a href="javascript:;">--}}
-          {{--<i class="icon-users"></i>--}}
-          {{--<span class="title">会员卡</span>--}}
-          {{--<span class="arrow"></span>--}}
-        {{--</a>--}}
-        {{--<ul class="sub-menu">--}}
-          {{--<li><a id="menu-membercard" href="{{ user_url('/') }}">会员卡管理</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">回复设置</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">通知管理</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">幻灯片</a></li>--}}
-        {{--</ul>--}}
-      {{--</li>--}}
-      {{--<li>--}}
-        {{--<a href="javascript:;">--}}
-          {{--<i class="icon-globe"></i>--}}
-          {{--<span class="title">微网站</span>--}}
-          {{--<span class="arrow"></span>--}}
-        {{--</a>--}}
-        {{--<ul class="sub-menu">--}}
-          {{--<li><a href="{{ user_url('/') }}">回复设置</a></li>--}}
-          {{--<li><a id="menu-template" href="{{ user_url('/') }}">微站模板</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">导航菜单</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">菜单样式</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">内容分类</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">幻灯片</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">全屏背景</a></li>--}}
-        {{--</ul>--}}
-      {{--</li>--}}
-      {{--<li>--}}
-        {{--<a href="javascript:;">--}}
-          {{--<i class="icon-basket"></i>--}}
-          {{--<span class="title">微商城</span>--}}
-          {{--<span class="arrow"></span>--}}
-        {{--</a>--}}
-        {{--<ul class="sub-menu">--}}
-          {{--<li><a href="{{ user_url('/') }}">商品分类</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">商品管理</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">订单管理</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">回复设置</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">店铺设置</a></li>--}}
-        {{--</ul>--}}
-      {{--</li>--}}
-      {{--<li>--}}
-        {{--<a href="javascript:;">--}}
-          {{--<i class="icon-credit-card"></i>--}}
-          {{--<span class="title">微信卡券</span>--}}
-          {{--<span class="arrow"></span>--}}
-        {{--</a>--}}
-        {{--<ul class="sub-menu">--}}
-          {{--<li><a href="{{ user_url('/') }}">卡券管理</a></li>--}}
-          {{--<li><a href="{{ user_url('/') }}">CODE管理</a></li>--}}
-        {{--</ul>--}}
-      {{--</li>--}}
-      <li>
-        <a href="javascript:;">
-          <i class="icon-frame"></i>
-          <span class="title">二维码</span>
-          <span class="arrow"></span>
-        </a>
-        <ul class="sub-menu">
-          <li><a href="{{ user_url('qrcode/') }}">二维码</a></li>
-        </ul>
-      </li>
-      <li>
-        <a href="javascript:;">
-          <i class="icon-doc"></i>
-          <span class="title">模板消息</span>
-          <span class="arrow"></span>
-        </a>
-        <ul class="sub-menu">
-          <li><a href="{{ user_url('/') }}">消息模板设置</a></li>
-          <li><a href="{{ user_url('/') }}">模板消息记录</a></li>
-        </ul>
-      </li>
-      <li>
-        <a href="javascript:;">
-          <i class="icon-bubbles"></i>
-          <span class="title">高级群发</span>
-          <span class="arrow"></span>
-        </a>
-        <ul class="sub-menu">
-          <li><a href="{{ user_url('/') }}">群发消息</a></li>
-          <li><a href="{{ user_url('/') }}">群发消息记录</a></li>
-        </ul>
-      </li>
-      <li class="last">
-        <a href="javascript:;">
-          <i class="icon-pie-chart"></i>
-          <span class="title">粉丝管理</span>
-          <span class="arrow"></span>
-        </a>
-        <ul class="sub-menu">
-          <li><a href="{{ user_url('fans/') }}">粉丝列表</a></li>
-          <li><a href="{{ user_url('fans-group/') }}">粉丝分组</a></li>
-        </ul>
-      </li>
-    </ul>
-    <!-- END SIDEBAR MENU -->
-  </div>
+        <!-- END SIDEBAR MENU -->
+    </div>
+    <!-- END SIDEBAR -->
 </div>
