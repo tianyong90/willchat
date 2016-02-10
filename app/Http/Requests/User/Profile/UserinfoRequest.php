a52d9dfec95123636d9d8f92993f4897bccacda4
tianyong90 <412039588@qq.com>
2016-02-10 20:01:19 +0800

    表单验证提示完善

--- app/Http/Requests/User/Profile/UserinfoRequest.php
+++ app/Http/Requests/User/Profile/UserinfoRequest.php
@@ -3,19 +3,10 @@
 namespace App\Http\Requests\User\Profile;
 
 use App\Http\Requests\Request;
+use App\Models\User;
 
 class UserinfoRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -28,12 +19,4 @@ class UserinfoRequest extends Request
             'mobile' => 'required',
         ];
     }
-
-//    public function messages()
-//    {
-//        return [
-//            'nickname.required' => '昵称必须填写',
-//            'mobile.required' => '手机号必须填写',
-//        ];
-//    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,20 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
         'password',
         'remember_token',
     ];
+
+    /**
+     * 用于表单验证时的字段名称提示.
+     *
+     * @var array
+     */
+    public static $aliases = [
+        'name' => '用户名',
+        'password' => '登录密码',
+        'nickname' => '昵称',
+        'email' => '邮箱',
+        'mobile' => '手机号',
+        'qq' => 'qq号',
+        'last_login_at' => '最后登录时间',
+        'last_login_ip' => '最后登录IP',
+    ];
 }
