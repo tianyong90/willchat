fe2875cbbbbbfe052923b705d52d0606b6205baa
tianyong90 <412039588@qq.com>
2016-02-23 15:31:08 +0800

    PostModel 完善

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,20 @@ class Post extends Model implements Transformable
 
     protected $fillable = [];
 
+    protected $dates = ['published_at'];
+
+    /**
+     * 根据title自动生成slug
+     *
+     * @param $value
+     */
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+
+        if (! $this->exists) {
+            $this->attributes['slug'] = str_slug($value);
+        }
+    }
+
 }
